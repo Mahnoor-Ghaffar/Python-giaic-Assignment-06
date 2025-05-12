04ad66340dceaed779c8ce8ecc10fa41263e4611
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-12 13:12:47 +0500

    Auto commit: 2025-05-12 13:12:47.401338

--- main.py
+++ main.py
@@ -254,3 +254,11 @@ class TemperatureConverter:
         return (c*9/5) +32
 
 print(TemperatureConverter.celsius_to_fahrenheit(25))
+
+
+
+# 13. Composition
+# Assignment:
+# Create a class Engine and a class Car.
+#  Use composition by passing an Engine object to the Car class during initialization. Access a method of the Engine class via the Car class.
+
