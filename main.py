d7f488a256940852b4acea7da95712f9817bb6a6
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-12 15:49:51 +0500

    Auto commit: 2025-05-12 15:49:51.663570

--- main.py
+++ main.py
@@ -353,3 +353,11 @@ def say_hello():
 say_hello()
 
 
+
+# 17. Class Decorators
+# Assignment:
+# Create a class decorator add_greeting that modifies a class to add a greet() method returning "Hello from Decorator!".
+# Apply it to a class Person.
+
+def add_greeting(cls):
+    cls.greet = lambda
