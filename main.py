881189a2b4cc6792a5450274a6f3877f19d47779
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-12 15:03:00 +0500

    Auto commit: 2025-05-12 15:03:00.591614

--- main.py
+++ main.py
@@ -285,3 +285,13 @@ c.start_car()
 # Create a class Department and a class Employee.
 # Use aggregation by having a Department object store a reference to an Employee object that exists independently of it.
 
+# -------Explanation
+
+# Aggregation mein ek object doosre object ka reference rakhta hai
+
+class Employee:
+    def __init__(self,name):
+        self.name = name
+
+class Department:
+    def __init__(self,emp)
