8cc48dacb41820a3f85d887d61984e380bb7d8f0
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-12 15:29:04 +0500

    Auto commit: 2025-05-12 15:29:04.527836

--- main.py
+++ main.py
@@ -332,3 +332,14 @@ class D(B,C):
 
 d = D()
 d.show()
+
+
+
+# 16. Function Decorators
+# Assignment:
+# Write a decorator function log_function_call that prints "Function is being called" before a function executes.
+# Apply it to a function say_hello().
+
+def log_function_call(func):
+    def wrapper():
+        print("Function is being called")
