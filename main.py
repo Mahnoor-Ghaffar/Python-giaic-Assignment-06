941022f335da379463f7fef2ded30daec0b0901e
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-12 16:00:05 +0500

    Auto commit: 2025-05-12 16:00:05.041168

--- main.py
+++ main.py
@@ -406,3 +406,57 @@ del p.price
 # Create a class Multiplier with an __init__() to set a factor.
 # Define a __call__() method that multiplies an input by the factor. Test it with callable() and by calling the object like a function.
 
+class Multiplier:
+    def __init__(self, factor):
+        self.factor = factor
+
+    def __call__(self, x):
+        return x * self.factor
+
+m = Multiplier(3)
+print(callable(m))  # True
+print(m(5))         # 15
+
+
+# 20. Creating a Custom Exception
+# Assignment:
+# Create a custom exception InvalidAgeError. 
+# Write a function check_age(age) that raises this exception if age < 18. Handle it with try...except.
+
+class InvalidAgeError(Exception):
+    pass
+
+def check_age(age):
+    if age < 18:
+        raise InvalidAgeError("Age must be 18 or above.")
+    else:
+        print("Age is valid.")
+
+try:
+    check_age(16)
+except InvalidAgeError as e:
+    print(e)
+
+
+
+# 21. Make a Custom Class Iterable
+# Assignment:
+# Create a class Countdown that takes a start number.
+# Implement __iter__() and __next__() to make the object iterable in a for-loop, counting down to 0.
+
+class Countdown:
+    def __init__(self, start):
+        self.start = start
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.start < 0:
+            raise StopIteration
+        current = self.start
+        self.start -= 1
+        return current
+
+for num in Countdown(5):
+    print(num)
