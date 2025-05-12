e660e018d5612c9397d712957f64a3802b526f5f
Mahnoor-Ghaffar <160515091+Mahnoor-Ghaffar@users.noreply.github.com>
2025-05-12 15:18:47 +0500

    Auto commit: 2025-05-12 15:18:47.665738

--- main.py
+++ main.py
@@ -300,3 +300,21 @@ class Department:
 e = Employee("Ali")
 d= Department(e)
 print(d.emp.name)
+
+
+
+# 15. Method Resolution Order (MRO) and Diamond Inheritance
+# Assignment:
+# Create four classes:
+
+# A with a method show(),
+
+# B and C that inherit from A and override show(),
+
+# D that inherits from both B and C.
+
+# Create an object of D and call show() to observe MRO.
+
+class A:
+    def show(self)
+
